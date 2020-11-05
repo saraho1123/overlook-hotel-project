2d059aecfdcb20a0584976859839afae0c8bfb93
Sarah Osgood <sarahjs117@gmail.com>
2020-11-04 19:30:38 -0600

    Add sample data for testing

---
+++ src/sample-data/sample-room-data.js
@@ -0,0 +1,9 @@
+const sampleRoomData = [
+  {"number": 1,"roomType": "residential suite","bidet": true,"bedSize": "queen","numBeds": 1,"costPerNight":358.4},
+  {"number": 2,"roomType": "suite","bidet": false,"bedSize":"full","numBeds":2,"costPerNight":477.38},
+  {"number":3,"roomType":"single room","bidet":false,"bedSize":"king","numBeds":1,"costPerNight":491.14},
+  {"number":4,"roomType":"junior suite","bidet":true,"bedSize":"queen","numBeds":1,"costPerNight":397.02},
+  {"number":5,"roomType":"single room","bidet":true,"bedSize":"twin","numBeds":2,"costPerNight":207.24}
+];
+
+export default sampleRoomData;
---
+++ src/sample-data/sample-user-data.js
@@ -0,0 +1,9 @@
+const sampleUserData = [
+  {"id": 1, "name": "Isaac Osgood"},
+  {"id": 2, "name":"David Osgood"},
+  {"id": 3,"name":"Joshua Osgood"},
+  {"id": 4,"name":"Little Buddy"},
+  {"id": 5,"name":"Silly Goosefloose"}
+];
+
+export default sampleUserData;
