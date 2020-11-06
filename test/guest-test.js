81cf812607c17c278e9941a48418ee3651faca30
Sarah Osgood <sarahjs117@gmail.com>
2020-11-05 18:43:13 -0600

    Add tests for seperatePastFromUpcomingBookings method on Guest class

--- test/guest-test.js
+++ test/guest-test.js
@@ -9,11 +9,51 @@ describe('Guest', () => {
   let guest;
 
   beforeEach( () => {
-    guest = new Guest();
+    guest = new Guest(sampleUserData, sampleRoomData, sampleBookingData);
   });
   
   it('should be an instance of Guest', () => {
     expect(guest).to.be.an.instanceof(Guest);
+  });
+
+  it('should be able to select a user by id from login', () => {
+    guest.selectGuest("id", 1);
+
+    expect(guest.selectedGuest.name).to.equal("Isaac Osgood");
+  });
+
+  it('should be able to find all bookings for selected guest', () => {
+    guest.selectGuest("Isaac Osgood");
+    guest.getSelectedGuestBookings()
+    expect(guest.selectedGuestBookings).to.deep.equal([  
+      {"id":"5fwrgu4i7k55hl6sz","userID":1,"date":"2020/04/22","roomNumber":1,"roomServiceCharges":[]},
+      {"id":"5fwrgu4i7k55hl6t9","userID":1,"date":"2020/04/21","roomNumber":5,"roomServiceCharges":[]},
+      {"id":"5fwrgu4i7k55hl6t0","userID":1,"date":"2020/04/21","roomNumber":1,"roomServiceCharges":[]},
+      {"id":"5fwrgu4i7k55hl6t3","userID":1,"date":"2020/05/21","roomNumber":3,"roomServiceCharges":[]},
+    ])
+  });
+
+  it('should be able to book a room for selected guest', () => {
+    expect(guest.bookRoomForGuest(1, '2020/11/14', 4)).to.deep.equal(
+      {
+        "userID": 1,
+        "date": "2020/11/14",
+        "roomNumber": 4
+    })
+  });
+
+  it('should be able to seperate past bookings from upcoming bookings', () => {
+    guest.selectGuest("id", 1);
+    guest.getSelectedGuestBookings();
+    guest.seperatePastFromUpcomingBookings('2020/4/30')
+    expect(guest.pastBookings).to.deep.equal([
+      {"id":"5fwrgu4i7k55hl6sz","userID":1,"date":"2020/04/22","roomNumber":1,"roomServiceCharges":[]},
+      {"id":"5fwrgu4i7k55hl6t9","userID":1,"date":"2020/04/21","roomNumber":5,"roomServiceCharges":[]},
+      {"id":"5fwrgu4i7k55hl6t0","userID":1,"date":"2020/04/21","roomNumber":1,"roomServiceCharges":[]},
+    ]);
+    expect(guest.upcomingBookings).to.deep.equal([
+      {"id":"5fwrgu4i7k55hl6t3","userID":1,"date":"2020/05/21","roomNumber":3,"roomServiceCharges":[]},
+    ]);
   });
 
 });
