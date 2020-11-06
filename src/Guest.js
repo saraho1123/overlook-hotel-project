d3a2402f26b6d0b0f25fb7a54d15a3ae87423c32
Sarah Osgood <sarahjs117@gmail.com>
2020-11-05 18:42:10 -0600

    Small edits to account for using Hotel parent class

--- src/Guest.js
+++ src/Guest.js
@@ -1,34 +1,37 @@
 import Hotel from '../src/Hotel';
 
 class Guest extends Hotel {
-  constructor(usersData, roomData, bookingsData) {
-    super(usersData, roomData, bookingsData)
-    this.guest = null;
-    this.users = usersData;
-    this.rooms = roomData;
-    this.bookings = bookingsData;
+  constructor(usersData, roomsData, bookingsData) {
+    super(usersData, roomsData, bookingsData)
+    // this.users = usersData
+    // this.rooms = roomsData;
+    // this.bookings = bookingsData;
+    // this.selectedGuest = null;
+    // this.selectedGuestBookings = null;
+      this.pastBookings = [];
+      this.upcomingBookings = [];
   }
 
-  // selectGuest(userKey, userIdentifier) {
-  //   this.selectedGuest = this.users.find(user => {
-  //     return user[userKey] === userIdentifier;
-  //   })
-  // }
+  seperatePastFromUpcomingBookings(date) {
+    let todaysDate = new Date(date)
+    return this.selectedGuestBookings.filter(booking => {
+      let bookingDate = new Date(booking.date)
+      let smallerDate = bookingDate.getTime() < todaysDate.getTime();
+      smallerDate ? this.pastBookings.push(booking) : this.upcomingBookings.push(booking);
+    })
+  }
 
-  // FOR GUEST CLASS!
-  // getGuestTotalSpent() {
-  //   return this.rooms.reduce((totalSpent, room) => {
-  //     this.bookings.forEach(booking => {
-  //       if (booking.roomNumber === room.number) {
-  //         totalSpent += room.costPerNight
-  //     }
-  //   })
-  //     return totalSpent
-  //   }, 0) 
-  // }
-  // */
-
-
+  getGuestTotalSpent() {
+    return this.rooms.reduce((totalSpent, room) => {
+      this.bookings.forEach(booking => {
+        if (booking.roomNumber === room.number) {
+          totalSpent += room.costPerNight
+      }
+    })
+      return totalSpent
+    }, 0) 
+  }
+  
 }
 
 export default Guest;
