519604f910dda51c4181529695bca4d27a779080
Sarah Osgood <sarahjs117@gmail.com>
2020-11-08 15:48:01 -0600

    Set up login logic

--- src/index.js
+++ src/index.js
@@ -16,10 +16,82 @@ import './images/san-juan-dawn.png'
 import './images/san-juan-day.png'
 import './images/san-juan-evening.png'
 import './images/snake-house-for-overlook.png'
+import Guest from './Guest';
+import Manager from './Manager';
+
+// GLOBALS
+let guest;
+let manager;
+let userData;
+let bookingData;
+let roomData;
+
+// Query Selectors
+
+// inputs
+const userLogin = document.querySelector('.user-login');
+const userPassword = document.querySelector('.user-password');
+
+// buttons
+const loginButton = document.querySelector('.submit-login');
+
+// page views
+const loginView = document.querySelector('.login-view');
+const navSection = document.querySelector('.nav-buttons-section');
+const guestHomeView = document.querySelector('.guest-home-view');
+
+// other elements
+const headingGuestName = document.querySelector('.heading-name');
+
+// Event Listeners
+window.addEventListener('keyup', validateUserLogin);
+loginButton.addEventListener('click', showHomeView);
+
+console.log('Time to really rock this project!');
+
+function enableSubmitButton() {
+  loginButton.classList.remove('disable-style');
+  loginButton.disabled = false;
+}
+function validateUserLogin() {
+  console.log('It works!');
+  if (userLogin.value === 'customer01' && userPassword.value === 'Overlook2020') {
+    enableSubmitButton();
+  } else if (userLogin.value === 'manager' && userPassword.value === 'Overlook2020') {
+    enableSubmitButton();
+  }
+}
+
+function showHomeView() {
+  if (userLogin.value === 'manager') {
+    enableManagerView();
+    manager = new Manager(usersData, roomsData, bookingsData);
+  } else {
+    guest = new Guest(usersData, roomsData, bookingsData);
+    enableGuestHomeView();
+  }
+}
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
 
+function enableGuestHomeView() {
+  loginView.classList.add('hidden');
+  navSection.classList.remove('hidden');
+  guestHomeView.classList.remove('hidden');
+  headingGuestName.value = '${user.name}'
+}
+
+function enableManagerView() {
+  loginView.classList.add('hidden');
+  managerView.classList.remove('hidden');
+}
+
+// validateUserPassword() {
+  // loginView.classList.add('hidden');
+  //   navSection.classList.remove('hidden');
+    // guestHomeView.classList.remove('hidden');
+
+// }
 /*
 
 l
